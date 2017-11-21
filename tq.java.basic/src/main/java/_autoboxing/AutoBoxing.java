ecc7782cf4af49361ae93d2bbff70e9e0f887dd0
t734070824 <t05575341338>
2017-11-21 14:08:58 +0800

    删除警告

--- tq.java.basic/src/main/java/_autoboxing/AutoBoxing.java
+++ tq.java.basic/src/main/java/_autoboxing/AutoBoxing.java
@@ -7,11 +7,10 @@ public class AutoBoxing {
 	public static void main(String[] args) {
 		Integer test1 = 233;
 		Integer test2 = 233;
-		boolean is = test1==test2;
 		System.out.println(test1==test2);
 		
 		Map<Integer, Integer> map = new HashMap<>();
 		int i = map.get(1);//nullPointEx;
-		
+		System.err.println(i);
 	}
 }
