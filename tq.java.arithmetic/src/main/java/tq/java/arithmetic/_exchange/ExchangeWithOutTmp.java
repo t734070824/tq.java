6807ede84fc6aafc634d0b425a6ce1d4e9e65039
t734070824 <t05575341338>
2017-11-28 16:05:28 +0800

    不使用临时变量交换整数

---
+++ tq.java.arithmetic/src/main/java/tq/java/arithmetic/_exchange/ExchangeWithOutTmp.java
@@ -0,0 +1,33 @@
+package tq.java.arithmetic._exchange;
+
+public class ExchangeWithOutTmp {
+	
+	public void exchangeXOR(int a, int b) {
+		System.err.println("a-->" + a + "  b-->" + b);
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+		System.err.println("a-->" + a + "  b-->" + b);
+	}
+	
+	//考虑溢出
+	public void exchangeXOR(ExchangeNum num) {
+		num.a = num.a + num.b;
+		num.b = num.a - num.b;
+		num.a = num.a - num.b;
+	}
+	
+	public static void main(String[] args) {
+		int a = Integer.MAX_VALUE;
+		int b = a - 1;
+		
+		ExchangeWithOutTmp e = new ExchangeWithOutTmp();
+		e.exchangeXOR(a, b);
+	}
+	
+}
+
+class ExchangeNum{
+	int a;
+	int b;
+}
