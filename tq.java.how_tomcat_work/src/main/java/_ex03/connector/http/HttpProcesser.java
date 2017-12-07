31f7c655d434c30bb35c6b906304a5d6eb97a27b
tangqing <734070824@qq.com>
2017-12-07 20:27:15 +0800

    深入剖析tomcat

--- tq.java.how_tomcat_work/src/main/java/_ex03/connector/http/HttpProcesser.java
+++ tq.java.how_tomcat_work/src/main/java/_ex03/connector/http/HttpProcesser.java
@@ -1,14 +1,15 @@
 package _ex03.connector.http;
 
-import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
+
+import javax.servlet.ServletException;
 
 public class HttpProcesser {
 	
 	HttpRequest request;
 	HttpResponse response;
-	HttpRequestLine requestLine;
+	private HttpRequestLine requestLine = new HttpRequestLine();
 
 	public HttpProcesser(HttpConnector httpConnector) {
 		
@@ -40,7 +41,7 @@ public class HttpProcesser {
 			}
 			
 			socket.close();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
@@ -50,8 +51,131 @@ public class HttpProcesser {
 		
 	}
 
-	private void parseRequest(SocketInputStream inputStream, OutputStream out) {
+	private void parseRequest(SocketInputStream inputStream, OutputStream out) throws ServletException {
 		inputStream.readRequestLine(requestLine);
+		String method = new String(requestLine.method, 0, requestLine.methodEnd);
+		String uri = null;
+		String protocol = new String(requestLine.protocol, 0, requestLine.protocolEnd);
+		if(method.length() < 1) {
+			throw new ServletException("Missing HTTP method");
+		}
+		if(requestLine.uriEnd < 1) {
+			throw new ServletException("Missing HTTP URI");
+		}
+		
+		int question = requestLine.indexof("?");
+		if(question >= 0) {
+			request.setQueryString(new String(requestLine.uri, question + 1, requestLine.uriEnd - question - 1));
+			uri = new String(requestLine.uri, 0, question);
+		}
+		
+		if(!uri.startsWith("/")) {
+			int pos = uri.indexOf("://");
+			if(pos != -1) {
+				pos = uri.indexOf('/', pos + 3);
+				if(pos == -1) {
+					uri = "";
+				} else {
+					uri = uri.substring(pos);
+				}
+			}
+		}
+		
+		String match = ";jsessionid=";
+		int semicolon = uri.indexOf(match);
+		if(semicolon >= 0) {
+			String rest = uri.substring(semicolon + match.length());
+			int semicolon2 = rest.indexOf(";");
+			if(semicolon >= 0) {
+				request.setRequestedSessionId(rest.substring(0, semicolon2));
+				rest = rest.substring(semicolon2);
+			} else {
+				request.setRequestedSessionId(rest);
+				rest = "";
+			}
+			request.setRequestedSessionURL(true);
+			
+			uri = uri.substring(0, semicolon) + rest;
+			
+		} else {
+			request.setRequestedSessionId(null);
+			request.setRequestedSessionURL(false);
+		}
+		
+		
+		String normalizedUri = normalize(uri);
+		request.setMethod(method);
+		request.setProtocol(protocol);
+		if(normalizedUri != null) {
+			request.setRequestURI(normalizedUri);
+		} else {
+			request.setRequestURI(uri);
+		}
+		
+		if(normalizedUri == null) {
+			throw new ServletException("Invalid URI:" + uri);
+		}
+		
+		
+		
+		
+	}
+
+	private String normalize(String uri) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tq.java.how_tomcat_work/src/main/java/_ex03/connector/http/HttpRequestLine.java
+++ tq.java.how_tomcat_work/src/main/java/_ex03/connector/http/HttpRequestLine.java
@@ -2,4 +2,23 @@ package _ex03.connector.http;
 
 public class HttpRequestLine {
 
+	public char[] method;
+	
+	public int methodEnd;
+
+	public int protocolEnd;
+
+	public char[] protocol;
+
+	public int uriEnd;
+
+	public char[] uri;
+
+	public int indexof(String string) {
+		return 0;
+	}
+
+
+
+	
 }
