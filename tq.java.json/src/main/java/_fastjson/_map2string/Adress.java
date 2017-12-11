9ce338a948a6a15f79ab107f9d576f3647ab3a2d
tangqing <734070824@qq.com>
2017-12-11 18:18:19 +0800

    json 包装类

--- tq.java.json/src/main/java/_fastjson/_map2string/Adress.java
+++ tq.java.json/src/main/java/_fastjson/_map2string/Adress.java
@@ -6,12 +6,17 @@ public class Adress {
 	
 	private String address;
 	
+	private Integer num;
+	
+	private int numInt;
+	
 	
 
-	public Adress(String name, String address) {
+	public Adress(String name, String address, Integer num) {
 		super();
 		this.name = name;
 		this.address = address;
+		this.num = num;
 	}
 
 	public String getName() {
@@ -28,6 +33,22 @@ public class Adress {
 
 	public void setAddress(String address) {
 		this.address = address;
+	}
+
+	public Integer getNum() {
+		return num;
+	}
+
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+
+	public int getNumInt() {
+		return numInt;
+	}
+
+	public void setNumInt(int numInt) {
+		this.numInt = numInt;
 	}
 	
 	
--- tq.java.json/src/main/java/_fastjson/_map2string/MapStringDemo.java
+++ tq.java.json/src/main/java/_fastjson/_map2string/MapStringDemo.java
@@ -7,7 +7,7 @@ import com.alibaba.fastjson.JSON;
 public class MapStringDemo {
 	
 	public static void main(String[] args) {
-		String jsonString = JSON.toJSONString(new Person("123", 1, new Adress("tq", "sss")));
+		String jsonString = JSON.toJSONString(new Person("123", 1, new Adress("tq", "sss", null)));
 		System.err.println(jsonString);
 		
 		Map<String, Object> mapTypes = JSON.parseObject(jsonString);
