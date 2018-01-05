c69c9234adeff6281f90795859f18173e4b2f483
tangqing <734070824@qq.com>
2018-01-05 18:16:04 +0800

    readme

--- tq.java.basic/src/main/java/_collection/_map/SameHashCode.java
+++ tq.java.basic/src/main/java/_collection/_map/SameHashCode.java
@@ -1,4 +1,27 @@
 package _collection._map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SameHashCode {
+
+    public static void main(String[] args) {
+        Map<PP, String> map = new HashMap<>();
+        PP p = new PP();
+        map.put(p, "ss");
+        PP pp = new PP();
+        map.put(pp, "ss");
+        System.err.println(pp == p);
+        System.err.println(map.get(p));
+        System.err.println(map.get(pp));
+        System.err.println(map);
+    }
+
+  public static  class PP {
+        @Override
+        public int hashCode() {
+            return 1;
+        }
+
+    }
 }
