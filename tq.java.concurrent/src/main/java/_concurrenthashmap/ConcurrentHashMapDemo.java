435ed0a06dac080a05b0fe7e4f0e28b311345d20
tangqing <734070824@qq.com>
2018-03-15 16:19:59 +0800

    concurrentHashMap 源码

--- tq.java.concurrent/src/main/java/_concurrenthashmap/ConcurrentHashMapDemo.java
+++ tq.java.concurrent/src/main/java/_concurrenthashmap/ConcurrentHashMapDemo.java
@@ -1,8 +1,19 @@
 package _concurrenthashmap;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
+ * ConcurrentHashMap 源码
  * @author 734070824@qq.com
  * @date 2018/3/13 17:56
  */
 public class ConcurrentHashMapDemo {
+
+    public static void main(String[] args) {
+        Map<String, String> cm = new ConcurrentHashMap<String, String>();
+        for (int i = 0; i < 14; i++) {
+            cm.put("key_" + i, "tangqing_" + i);
+        }
+    }
 }
