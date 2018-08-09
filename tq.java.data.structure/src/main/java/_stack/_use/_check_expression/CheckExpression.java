ac554eee25b70d6046f9cf7f7126a178bcbb9cb0
tangqing <734070824@qq.com>
2018-08-09 16:55:06 +0800

    括号匹配

--- tq.java.data.structure/src/main/java/_stack/_use/_check_expression/CheckExpression.java
+++ tq.java.data.structure/src/main/java/_stack/_use/_check_expression/CheckExpression.java
@@ -18,31 +18,50 @@ public class CheckExpression {
 
         String expstr3="(5-3)*8-2)";
         System.out.println(expstr3+"  "+isValid(expstr3));
+
+
+        System.out.println(isValid("{[()]()[{}]}"));
+        System.out.println(isValid("{[()]}}"));
     }
 
 
     public static String isValid(String expstr){
-        SeqStack<String> stack = new SeqStack<>();
+        SeqStack<Character> stack = new SeqStack<>();
         int index = 0;
         while (index < expstr.length() ){
             char c = expstr.charAt(index++);
             switch (c){
-                case '(':
-                    // 左括号直接push
-                    stack.push(c + "");
-                    break;
                 case ')':
-                    //一个有括号必须对应一个左括号
-                    if(stack.isEmpty() || !stack.pop().equals("(")) return "(";
+                    //一个右括号必须对应一个左括号
+                    if(stack.isEmpty() || !stack.pop().equals('(')){
+                        return "(";
+                    }
+                    break;
+                case ']':
+                    if(stack.isEmpty() || !stack.pop().equals('[')){
+                        return "[";
+                    }
+                    break;
+                case '}':
+                    if(stack.isEmpty() || !stack.pop().equals('{')){
+                        return "{";
+                    }
+                    break;
+                case '(': case '[': case '{':
+                    stack.push(c);
+                    break;
                 default:
+
                     break;
             }
         }
         //最后检测是否为空,为空则检测通过
-        if(stack.isEmpty())
+        if(stack.isEmpty()){
             return "check pass!";
-        else
+        }else{
             return "check exception!";
+        }
+
     }
 
 
