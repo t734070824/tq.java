75d0b0a2d95d26787c501f38a3ea055887759405
tangqing <734070824@qq.com>
2018-08-10 17:23:06 +0800

    试探回溯 八皇后

---
+++ tq.java.data.structure/src/main/java/_book/_4_stack/_eight_queen/PlaceQueens.java
@@ -0,0 +1,8 @@
+package _book._4_stack._eight_queen;
+
+/**
+ * @author 734070824@qq.com
+ * @date 2018/8/10 16:25
+ */
+public class PlaceQueens {
+}
---
+++ tq.java.data.structure/src/main/java/_book/_4_stack/_eight_queen/Queen.java
@@ -0,0 +1,8 @@
+package _book._4_stack._eight_queen;
+
+/**
+ * @author 734070824@qq.com
+ * @date 2018/8/10 15:38
+ */
+public class Queen {
+}
