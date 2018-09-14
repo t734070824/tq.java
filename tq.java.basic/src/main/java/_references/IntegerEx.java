dce2032522bb0bd31f7045ff69c919bcf33c01dc
tangqing <734070824@qq.com>
2018-09-14 17:50:35 +0800

    内存模型

--- tq.java.basic/src/main/java/_references/IntegerEx.java
+++ tq.java.basic/src/main/java/_references/IntegerEx.java
@@ -1,8 +1,22 @@
 package _references;
 
 /**
+ * 交换Integer
  * @author 734070824@qq.com
  * @date 2018/9/14 16:46
  */
 public class IntegerEx {
+
+    public static void main(String[] args) {
+        Integer l1 = new Integer(1);
+        Integer l2 = new Integer(2);
+        exchange(l1, l2);
+        System.err.println(l1 + "--" + l2);
+    }
+
+    private static void exchange(Integer l1, Integer l2) {
+        Integer tmp = l1;
+        l1 = l2;
+        l2 = tmp;
+    }
 }
