df0d63269953df047092f3673207ac30dc113263
tangqing <734070824@qq.com>
2018-10-08 19:01:58 +0800

    springboot 入门

---
+++ tq.java.spingboot/src/main/java/_reference_guide/_example/_myapplication/Application.java
@@ -0,0 +1,12 @@
+package _reference_guide._example._myapplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
