94ed1b7759dc66435b88a6a52c4163f2f1a5168f
tangqing <734070824@qq.com>
2018-11-21 20:12:06 +0800

    Linux 目录配置

---
+++ tq.java.basic/src/main/java/_collection/_list/AddIndex.java
@@ -0,0 +1,19 @@
+package _collection._list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 734070824@qq.com
+ * @date 2018/11/21 15:32
+ */
+public class AddIndex {
+
+    public static void main(String[] args) {
+        List<String> list= new ArrayList<>(4);
+
+        list.add(2, "ss");
+
+        //TODO 往后移动
+    }
+}
---
+++ tq.java.basic/src/main/java/_regular/App.java
@@ -0,0 +1,34 @@
+package _regular;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author 734070824@qq.com
+ * @date 2018/11/21 14:21
+ */
+public class App {
+
+    static Pattern javaW = Pattern.compile("debug$");
+
+    static Pattern debug = Pattern.compile("^[dc|ct].*debug$");
+
+    static Pattern release = Pattern.compile("^[dc|ct].*release$");
+
+
+    public static void main(String[] args) {
+        //返回true
+        System.err.println(Pattern.matches("\\d+","2223"));
+
+        //dc或ct开头的项目  debug 结尾则报测试版告警群 release 结尾则报正式版告警群
+
+        System.err.println(javaW.matcher("123.debug123").find());
+
+        System.err.println(javaW.matcher("123.debug123").find());
+
+        System.err.println(debug.matcher("ct.123.debug").find());
+        System.err.println(debug.matcher("dc.123.debug").find());
+
+        System.err.println(release.matcher("ct.123.release").find());
+        System.err.println(release.matcher("dc.123.release").find());
+    }
+}
