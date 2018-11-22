48581d74d5382d3561d87bfbb46932418740dc6d
tangqing <734070824@qq.com>
2018-11-22 17:10:42 +0800

    tcp/ip概述

--- tq.java.tcp.ip/src/main/java/_book/_tcp_ip_illustrated/_vol_1_the_protocols/Note.java
+++ tq.java.tcp.ip/src/main/java/_book/_tcp_ip_illustrated/_vol_1_the_protocols/Note.java
@@ -7,8 +7,11 @@ package _book._tcp_ip_illustrated._vol_1_the_protocols;
 public class Note {
 
     //书签
-    // TCP-IP详解(卷一、二、三) 83 / 1544
-    //TODO 第9 章IP 选路
-    //TODO 20180522 选路看着很痛苦
-    //TODO 先把17章 TCP 啃掉
+        // TCP-IP详解(卷一、二、三) 跳过第2章 15 / 1544
+        //
+
+    //TODO
+        //TODO 第9 章IP 选路
+        //TODO 20180522 选路看着很痛苦
+        //TODO 先把17章 TCP 啃掉
 }
