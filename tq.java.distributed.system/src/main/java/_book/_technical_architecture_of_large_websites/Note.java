8f099ac68d610139ce808dd3ab39a82d11324453
tangqing <734070824@qq.com>
2018-12-15 15:33:20 +0800

    高可用架构

--- tq.java.distributed.system/src/main/java/_book/_technical_architecture_of_large_websites/Note.java
+++ tq.java.distributed.system/src/main/java/_book/_technical_architecture_of_large_websites/Note.java
@@ -5,4 +5,12 @@ package _book._technical_architecture_of_large_websites;
  * @date 2018/12/15 15:15
  */
 public class Note {
+
+    //书签--大型网站技术架构 核心原理与案例分析
+        // 95 (119 / 242) 第六章 20181215
+        // 跳过 6,7,8
+        //
+
+
+    //TODO
 }
