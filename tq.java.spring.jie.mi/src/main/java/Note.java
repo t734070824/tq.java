39b98f723741bac820a0aa4af44d40b653497457
tangqing <734070824@qq.com>
2019-01-07 11:04:55 +0800

    spring 复习

--- tq.java.spring.jie.mi/src/main/java/Note.java
+++ tq.java.spring.jie.mi/src/main/java/Note.java
@@ -12,9 +12,14 @@ public class Note {
         // 305 / 673 20181016 dataSource, 跳过了 JdbcTemplate 大部分内容
         // 340 / 673 20181018 ibatis
         // 391 / 673 20181021 PlatformTransactionManager
+
+    //复习
+        // 20190107 _10_spring_aop_process_two
+
     //TODO
         //TODO 了解 component-scan 中include-filter 的作用
         //TODO Spring 分布式事务 jta
         //TODO ioc-aop-transaction 总结
         // 用到的设计模式与具体的类
+
 }
--- tq.java.spring.jie.mi/src/main/java/_1_spring_origin/thinking.md
+++ tq.java.spring.jie.mi/src/main/java/_1_spring_origin/thinking.md
@@ -3,5 +3,6 @@
 ## 思考
 1. AOP 如何与 IOC容器结合在一起的
     - TODO
+    - 动态生成代理类
 2. Spring MVC 与 AOP 没有关系??
     - TODO
