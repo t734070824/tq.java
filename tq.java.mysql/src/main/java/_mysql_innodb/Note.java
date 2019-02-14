1cc38db6e7db381265066a4b831627e2ba2e539a
tangqing <734070824@qq.com>
2019-02-14 16:06:10 +0800

    bin-log

--- tq.java.mysql/src/main/java/_mysql_innodb/Note.java
+++ tq.java.mysql/src/main/java/_mysql_innodb/Note.java
@@ -8,6 +8,7 @@ public class Note {
 
     //书签
         // Mysql 技术内幕 39 / 437 2019-01-24 CheckPoint
+        // Mysql 技术内幕 86 / 437 2019-02-14 bin-log
 
     //TODO
         //TODO BTree B+Tree 区别与深入
