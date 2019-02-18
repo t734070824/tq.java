20c6b69156e0ddee790af6bfcf40a85a923372ba
tangqing <734070824@qq.com>
2019-02-18 20:06:05 +0800

    全文索引初识

--- tq.java.mysql/src/main/java/_mysql_innodb/Note.java
+++ tq.java.mysql/src/main/java/_mysql_innodb/Note.java
@@ -10,8 +10,10 @@ public class Note {
         // Mysql 技术内幕 39 / 437 2019-01-24 CheckPoint
         // Mysql 技术内幕 86 / 437 2019-02-14 bin-log
         // Mysql 技术内幕 2019-02-17 第四章 跳过
+        // Mysql 技术内幕 262 / 437 2019-02-18 全文索引粗略读
 
 
     //TODO
         //TODO BTree B+Tree 区别与深入
+        //TODO 全文索引
 }
