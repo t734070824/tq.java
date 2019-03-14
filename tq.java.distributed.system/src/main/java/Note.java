3d0cc04b8fe62c1581e6e280397754bb27f5183f
tangqing <734070824@qq.com>
2019-03-14 13:40:29 +0800

    Snowflake

--- tq.java.distributed.system/src/main/java/Note.java
+++ tq.java.distributed.system/src/main/java/Note.java
@@ -12,6 +12,10 @@ public class Note {
     //TODO raft 一致性算法 http://thesecretlivesofdata.com/raft/  https://raft.github.io/
     //TODO 虚ip ip漂移
     //TODO Jmeter 压力测试工具
+    //TODO 故障转移
+    //TODO 全局时钟 vs 逻辑时钟, Vector Clock
+    //TODO GPS 原子钟
+    //TODO 脑裂
 
 
 }
