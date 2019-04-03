e210c95d861c0665caaa8b931a7b54728657db6c
tangqing <734070824@qq.com>
2019-04-03 19:01:43 +0800

    kafka 压缩 待学

--- tq.java.spring.boot/src/main/java/_blog_720ui_com/_1_fast_begin/RestfulApiWebDemo.java
+++ tq.java.spring.boot/src/main/java/_blog_720ui_com/_1_fast_begin/RestfulApiWebDemo.java
@@ -19,35 +19,55 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class RestfulApiWebDemo {
-    @RequestMapping(value = {"/{project}/api/{rest}", "/{project}/api/{app}/{rest}"}, method = RequestMethod.GET)
-    String home(HttpServletRequest request) {
-        System.err.println("haha-->" + request.getRequestURL());
-        System.err.println("haha-->" + request.getRequestURI());
-        System.err.println("haha-->" + request.getContextPath());
-        System.err.println("haha-->" + request.getServletPath());
-        System.err.println("haha-->" + request.getQueryString());
+//    @RequestMapping(value = {"/{project}/api/{rest}", "/{project}/api/{app}/{rest}"}, method = RequestMethod.GET)
+//    String home(HttpServletRequest request) {
+//        System.err.println("haha-->" + request.getRequestURL());
+//        System.err.println("haha-->" + request.getRequestURI());
+//        System.err.println("haha-->" + request.getContextPath());
+//        System.err.println("haha-->" + request.getServletPath());
+//        System.err.println("haha-->" + request.getQueryString());
+//        return "Hello World!";
+//    }
+//
+//    @RequestMapping(value = {"/{project}/api/{rest}", "/{project}/api/{app}/{rest}"}, method = RequestMethod.POST)
+//    String home2(HttpServletRequest request, @RequestBody Object body) {
+//        System.err.println("haha-->" +request.getRequestURL());
+//        System.err.println("haha-->" +request.getRequestURI());
+//        System.err.println("haha-->" +request.getContextPath());
+//        System.err.println("haha-->" +request.getServletPath());
+//        System.err.println("haha-->" +request.getQueryString());
+//        System.err.println("haha-->" + body);
+//        return "Hello World!";
+//    }
+//
+//    @RequestMapping(value = {"asda/api/sd", "/asda/api/ios/asd"}, method = RequestMethod.POST)
+//    String home3(HttpServletRequest request, @RequestBody Object body) {
+//        System.err.println("home3 haha-->" +request.getRequestURL());
+//        System.err.println("home3 haha-->" +request.getRequestURI());
+//        System.err.println("home3 haha-->" +request.getContextPath());
+//        System.err.println("home3 haha-->" +request.getServletPath());
+//        System.err.println("home3 haha-->" +request.getQueryString());
+//        System.err.println("home3 haha-->" + body);
+//        return "Hello World!";
+//    }
+
+    @RequestMapping(value = {"/**"}, method = RequestMethod.GET)
+    String home4(HttpServletRequest request) {
+        System.err.println("home4-->" + request.getRequestURL());
+        System.err.println("home4-->" + request.getRequestURI());
+        System.err.println("home4-->" + request.getContextPath());
+        System.err.println("home4-->" + request.getServletPath());
+        System.err.println("home4-->" + request.getQueryString());
         return "Hello World!";
     }
 
-    @RequestMapping(value = {"/{project}/api/{rest}", "/{project}/api/{app}/{rest}"}, method = RequestMethod.POST)
-    String home2(HttpServletRequest request, @RequestBody Object body) {
-        System.err.println("haha-->" +request.getRequestURL());
-        System.err.println("haha-->" +request.getRequestURI());
-        System.err.println("haha-->" +request.getContextPath());
-        System.err.println("haha-->" +request.getServletPath());
-        System.err.println("haha-->" +request.getQueryString());
-        System.err.println("haha-->" + body);
-        return "Hello World!";
-    }
-
-    @RequestMapping(value = {"asda/api/sd", "/asda/api/ios/asd"}, method = RequestMethod.POST)
-    String home3(HttpServletRequest request, @RequestBody Object body) {
-        System.err.println("home3 haha-->" +request.getRequestURL());
-        System.err.println("home3 haha-->" +request.getRequestURI());
-        System.err.println("home3 haha-->" +request.getContextPath());
-        System.err.println("home3 haha-->" +request.getServletPath());
-        System.err.println("home3 haha-->" +request.getQueryString());
-        System.err.println("home3 haha-->" + body);
+    @RequestMapping(value = {"/**"}, method = RequestMethod.POST)
+    String home5(HttpServletRequest request) {
+        System.err.println("home5-->" + request.getRequestURL());
+        System.err.println("home5-->" + request.getRequestURI());
+        System.err.println("home5-->" + request.getContextPath());
+        System.err.println("home5-->" + request.getServletPath());
+        System.err.println("home5-->" + request.getQueryString());
         return "Hello World!";
     }
 
