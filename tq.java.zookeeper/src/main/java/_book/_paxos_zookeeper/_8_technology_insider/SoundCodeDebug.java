2aaef4a75f6e20eab77cf26ff2c4654218b1bfc1
tangqing <734070824@qq.com>
2019-04-21 14:10:45 +0800

    zookeeper 源码调试方法

---
+++ tq.java.zookeeper/src/main/java/_book/_paxos_zookeeper/_8_technology_insider/SoundCodeDebug.java
@@ -0,0 +1,50 @@
+package _book._paxos_zookeeper._8_technology_insider;
+
+import _book._paxos_zookeeper._5_use_zookeeper.Zookeeper_Create_Sync;
+import org.apache.zookeeper.*;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 调试方式
+ * https://zhuanlan.zhihu.com/p/45823132
+ * @author 734070824@qq.com
+ * @date 2019/4/19 10:57
+ */
+public class SoundCodeDebug implements Watcher {
+
+    private  static CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+        ZooKeeper zooKeeper = new ZooKeeper("192.168.7.194:2181", 5000, new SoundCodeDebug());
+        System.err.println(zooKeeper.getState());
+
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String path1 = zooKeeper.create("/zk-test-java",
+                "".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.PERSISTENT);
+        System.err.println("create: " +path1);
+
+        TimeUnit.SECONDS.sleep(1000);
+
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        System.err.println("Event:" + event);
+        if(Watcher.Event.KeeperState.SyncConnected == event.getState()){
+            countDownLatch.countDown();
+        }
+
+    }
+
+
+}
--- tq.java.zookeeper/src/main/java/_book/_paxos_zookeeper/thinking.md
+++ tq.java.zookeeper/src/main/java/_book/_paxos_zookeeper/thinking.md
@@ -7,3 +7,22 @@
     - 排它锁
         - 如何解决 我创建锁最后只会由我来删除
             - Version ? 
+2. PrepRequestProcessor 是多线程的吗
+    - 应该不是, 处理内部都没有加锁
+    - TODO
+1. processer 链条
+    - 常用
+        - PreRequestProcessor --> SyncRequestProcessor --> FinalRequestProcessor
+    - 有什么好处吗
+1. 一致性如何保证的? 也就是说 什么时候写本地日志
+    - TODO
+2. 客户端错误是如何返回的锁
+    - TODO
+3. Watcher 具体实现
+    - TODO
+    - 为什么在触发 Watcher 之后, 不降修改后的数据直接给客户端, 而需要客户端主动请求
+        - 既然注册了 Watcher, 说明客户端对这个节点的变化感兴趣啊
+        - TODO
+1. 理解 惊群效应 
+    - 大量Watcher 一个时间点一起触发
+    - 锁优化
