c737799af884dd79e1559aeff2109399ae7a14cb
tangqing <734070824@qq.com>
2020-08-21 09:57:07 +0800

    remove

--- tq.java.basic/src/main/java/_io/ReadLineApp.java
+++
@@ -1,8 +0,0 @@
-package _io;
-
-/**
- * @author 734070824@qq.com
- * @date 2018/5/25 11:20
- */
-public class ReadLineApp {
-}
--- tq.java.basic/src/main/java/_jdbc/Note.java
+++
@@ -1,6 +0,0 @@
-package _jdbc;
-
-public class Note {
-
-    //TODO 熟悉常见的数据库连接池 DPCP
-}
